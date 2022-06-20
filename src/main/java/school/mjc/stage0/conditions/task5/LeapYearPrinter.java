abf11d390a7c43c1266d73b3b0bd249b069c93a8
Romchik98 <38857653+Romchik98@users.noreply.github.com>
2022-06-20 16:19:06 +0300

    Initial commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year) {
+            case (2000):
+            case (2004):
+            case (2008):
+            case (2012):
+            case (2016):
+            case (2020):
+            case (2024):
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
